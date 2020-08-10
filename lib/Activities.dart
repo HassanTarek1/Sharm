9825d0c25316c249c13319b1e24abec356a8207a
Hassan Tarek Abu Helal <hassantarek201517@gmail.com>
2020-08-10 20:01:49 +0200

    initiation

---
+++ lib/Activities.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+
+class Activities extends StatefulWidget {
+  @override
+  _ActivitiesState createState() => _ActivitiesState();
+}
+
+class _ActivitiesState extends State<Activities> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(10, 30, 10, 30),
+            decoration: BoxDecoration(
+              color:  const Color.fromRGBO(233, 75, 69,1),
+              borderRadius: new BorderRadius.only(
+                bottomLeft: const Radius.circular(40.0),
+                bottomRight: const Radius.circular(40.0),
+              )
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+
+                  children: <Widget>[
+                    FlatButton.icon(onPressed: (){}, icon: Icon(
+                      Icons.arrow_back,
+                      color: Colors.white,
+                    ), label: Text('')),
+                    SizedBox(width: 210,),
+                    FlatButton.icon(onPressed:() {}, icon: Icon(
+                      Icons.menu,
+                      color: Colors.white,
+                    ), label: Text(''),),
+
+                  ],
+                ),
+                SizedBox(height: 40,),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(30, 0, 0, 10),
+                  child: Text('Activites',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontFamily: 'Garamond',
+                    color: Colors.white
+                  ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              FlatButton(
+                onPressed: (){},
+                child: Text('CITY TOUR PLAN',
+                style: TextStyle(fontSize: 16),),
+              ),
+              FlatButton(
+                onPressed: (){},
+                child: Text('ONE DAY TRIP',
+                style: TextStyle(
+                  fontSize: 16
+                ),),
+              ),
+              FlatButton(
+                onPressed: (){},
+                child: Text('NIGHT LIFE',
+                style: TextStyle(fontSize: 16),),
+              ),
+            ],
+          ),
+          Expanded(
+            child: SizedBox(
+              child: ListView(
+                  children: [
+                    GestureDetector(
+                      onTap: (){},
+                      child: Card(
+                          color: Colors.grey,
+
+                          child: Stack(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            children: <Widget>[
+                              Image.asset('assets/restaurant.jpg'),
+                              Container(
+                                margin: EdgeInsets.all(10),
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [Colors.white, Colors.amber]),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Text('RESTAURANT',
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                  ),),
+                              ),
+                            ],
+                          )
+                      ),
+
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Card(
+                          color: Colors.grey,
+
+                          child: Stack(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            children: <Widget>[
+                              Image.asset('assets/mountain dinner.jpg'),
+                              Container(
+                                margin: EdgeInsets.all(10),
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [Colors.white, Colors.amber]),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Text('MOUNTAIN DINNER',
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                  ),),
+                              ),
+                            ],
+                          )
+                      ),
+
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Card(
+                          color: Colors.grey,
+
+                          child: Stack(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            children: <Widget>[
+                              Image.asset('assets/naamabay.jpg'),
+                              Container(
+                                margin: EdgeInsets.all(10),
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [Colors.white, Colors.amber]),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Text('NAAMA BAY',
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                  ),),
+                              ),
+                            ],
+                          )
+                      ),
+
+                    ),
+                    GestureDetector(
+                      onTap: (){},
+                      child: Card(
+                          color: Colors.grey,
+                          child: Stack(
+                            alignment: AlignmentDirectional.bottomEnd,
+                            children: <Widget>[
+                              Image.asset('assets/clubs.jpg'),
+                              Container(
+                                margin: EdgeInsets.all(10),
+                                padding: EdgeInsets.all(5),
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(colors: [Colors.white, Colors.amber]),
+                                    borderRadius: BorderRadius.circular(5)
+                                ),
+                                child: Text('PUBS & CLUBS',
+                                  style: TextStyle(
+                                    fontSize: 26,
+                                  ),),
+                              ),
+                            ],
+                          )
+                      ),
+
+                    )
+                  ]
+              ),
+            ),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            decoration: BoxDecoration(
+              color:  const Color.fromRGBO(233, 75, 69,1),
+              borderRadius: new BorderRadius.only(
+                topLeft: const Radius.circular(25.0),
+                topRight: const Radius.circular(25.0),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  padding: EdgeInsets.all(15),
+                  icon: Icon(Icons.home, size: 30, color: Colors.white),
+                  onPressed: (){},
+                ),
+                SizedBox(width: 30,),
+                IconButton(
+                  padding: EdgeInsets.all(15),
+                  icon: Icon(Icons.notifications, size: 30, color: Colors.white),
+                  onPressed: (){},
+                ),
+                SizedBox(width: 30,),
+                IconButton(
+                  padding: EdgeInsets.all(15),
+                  icon: Icon(Icons.location_on, size: 30, color: Colors.white),
+                  onPressed: (){},
+                ),
+                SizedBox(width: 30,),
+                IconButton(
+                  padding: EdgeInsets.all(15),
+                  icon: Icon(Icons.search, size: 30, color: Colors.white),
+                  onPressed: (){},
+                ),
+                SizedBox(width: 30,),
+                IconButton(
+                  padding: EdgeInsets.all(15),
+                  icon: Icon(Icons.account_circle, size: 30, color: Colors.white,),
+                  onPressed: (){},
+                ),
+              ],
+            ),
+
+          )
+        ],
+      ),
+    );
+  }
+}
